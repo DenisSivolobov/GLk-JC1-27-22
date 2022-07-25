53820ac5a24ec6b4016d3ca3af65e7df6847d5e3
DenisSivolobov <denisby90@gmail.com>
2022-07-25 21:43:00 +0300

    lesson1

---
+++ src/by/academy/lesson1/HelloWorld.java
@@ -0,0 +1,88 @@
+package by.academy.lesson1;
+
+import java.sql.SQLOutput;
+
+public class HelloWorld {
+   private byte a = 1, a1 = 12;
+    private short b = 2, bb = 15;
+    private int c = 3, cc = 11;
+    private long d = 4, dd = 55;
+    private float e = 5.1f, ee = 4.33f;
+    private double f = 6.21d, ff = 6.55d;
+    private boolean g = true, gg = false;
+    private char h = 'm', hh= 0x6d;
+
+
+    private Byte mm = 111;
+    private Short nn = 122;
+    private Integer gjk = 117;
+    private Long ttt= 1212L;
+    private Float rrr = 121.1F;
+    private Double eee = 121.33;
+    private Boolean iii = true;
+    private Character ooo = 121;
+
+
+
+    public byte getA() {
+        return a;
+    }
+    public short getB() {
+        return b;
+    }
+    public int getC() {
+        return c;
+    };
+    public long getD() {
+        return d;
+    }
+    public float getE() {
+        return e;
+    }
+    public double getF() {
+        return f;
+    }
+    public boolean isG() {
+        return G;
+    }
+    public char getH() {
+        return h;
+    }
+
+
+
+    public void setA(byte a) {
+        this.a = a11;
+    }
+
+    public void setB(short b) {
+        this.b = b11;
+    }
+
+    public void setC(int c) {
+        this.c = c2;
+    }
+
+    public void setD(long d) {
+        this.d = d11;
+    }
+
+    public void setE(float e) {
+        this.e = e11;
+    }
+
+    public void setF(double f) {
+        this.f = f11;
+    }
+
+    public void setG(boolean g) {
+        this.g = g11;
+    }
+
+    public void setH(char h) {
+        this.h = h11;
+    }
+
+    }
+
+
---
+++ src/by/academy/lesson1/MyClass.java
@@ -0,0 +1,55 @@
+package by.academy.lesson1;
+
+public class MyClass {
+    byte i = 10;
+    short j = 20;
+    int k = 30;
+    long l = 40;
+    float m = 50.1f;
+    double o = 60.21d;
+    boolean p = true;
+    char r = 'm';
+    private Byte mm = 111;
+    private Short nn = 122;
+    private Integer gjk = 117;
+    private Long ttt= 1212L;
+    private Float rrr = 121.1F;
+    private Double eee = 121.33;
+    private Boolean iii = true;
+    private Character ooo = 121;
+
+
+    public static void main(String[] args) {
+        byte ii = 24;
+        short jj = 320;
+        int kk = 300;
+        long ll = 4000;
+        float mm = 500.1f;
+        double oo = 607.21d;
+        boolean pp = false;
+        char rr = 'm';
+        Byte mmm = 111;
+        Short nn = 122;
+        Integer gjk = 117;
+        Long ttt= 1212L;
+        Float rrr = 121.1F;
+        Double eee = 121.33;
+        Boolean iii = true;
+        Character ooo = 121;
+
+        System.out.println(i, j, k); //переменных много, чтобы их вывести на консоль нужно все перечислять?
+
+
+        int x = 99;
+        double z = 10.5;
+        x = (int) z;
+        System.out.println(x);
+
+        double y = 123.1;
+        double y2 = 120.1;
+        int xw = 4;
+        xw = (int) (y/y2);
+        System.out.println(xw);
+
+    }
+}
