24fb49768af27ebb3270a898355c281befe2a41d
DenisSivolobov <denisby90@gmail.com>
2022-08-07 15:45:36 +0300

    lesson 4

--- src/by/academy/lesson4/Main1.java
+++ src/by/academy/lesson4/Main1.java
@@ -6,21 +6,41 @@ public class Main1 {
 
     public static void main(String[] args) {
 
+
         int matrix[][] = new int[10][10];
+        Random r = new Random();
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = (int)(i + Math.random());
-
-
-                Random random = new Random();
-                int nName = random.nextInt();
-                System.out.println(nName);
-            }
-
+            for (int j = 0; j < matrix[i].length; j++)
+                matrix[i][j] = r.nextInt(1000000);
         }
 
 
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+
+
+        }
+
+        String[] matr = new String[10];
+
+        Random rand = new Random();
+        for (int i = 0; i < matr.length; i++) {
+            char x = (char) rand.nextInt('a', 'z');
+            System.out.println(matr[i]);
+
+        }
+
     }
-
-
 }
+
+
+
+
+
+
+
+
+
--- src/by/academy/lesson4/Matrix.java
+++ src/by/academy/lesson4/Matrix.java
@@ -13,7 +13,7 @@ public class Matrix {
     public String gString() {
         Random random = new Random();
         StringBuilder s = new StringBuilder();
-        for (int i = 0; i < 7; i++) {
+        for (int i = 0; i < 10; i++) {
             char x = (char) random.nextInt('a', 'z');
         }
         return s.toString();
